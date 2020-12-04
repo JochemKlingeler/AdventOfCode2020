59d5ada578d1f7ea936574b1031fef070e434f7a
Jochem Klingeler <jochem.klingeler@gmail.com>
2020-12-04 13:07:49 +0100

    Day 1 complete

---
+++ README.md
@@ -0,0 +1,5 @@
+# Advent of code 2020
+
+This is my attempt to better understand what it means to program in Rust.
+
+No attempt is made to make the best solutions, just ones that work.
---
+++ day-01/src/lib.rs
@@ -0,0 +1,45 @@
+pub fn part1(input: &Vec<i32>) -> Result<i32, String> {
+    for a in input {
+        for b in input {
+            if 2020 == (a + b) {
+                return Ok(a * b);
+            }
+        }
+    }
+    Err(String::from("No valid values!"))
+}
+
+pub fn part2(input: &Vec<i32>) -> Result<i32, String> {
+    for a in input {
+        for b in input {
+            for c in input {
+                if 2020 == (a + b + c) {
+                    return Ok(a * b * c);
+                }
+            }
+        }
+    }
+    Err(String::from("No valid values!"))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_demo() {
+        let result = part1(&make_demo_input());
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), 514579);
+    }
+    #[test]
+    fn part2_demo() {
+        let result = part2(&make_demo_input());
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), 241861950);
+    }
+
+    fn make_demo_input() -> Vec<i32> {
+        return vec![1721, 979, 366, 299, 675, 1456];
+    }
+}
