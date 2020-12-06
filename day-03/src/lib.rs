50d7f96a25698262a0be8e180faa256c20e9fc45
Jochem Klingeler <jochem.klingeler@gmail.com>
2020-12-06 13:24:21 +0100

    Day 3, part 1 tested

---
+++ day-03/src/lib.rs
@@ -0,0 +1,48 @@
+pub fn part1(map: &Vec<String>, go_right: usize, go_down: usize) -> usize {
+    let mut x: usize = 0;
+    let mut y: usize = 0;
+    let mut trees: usize = 0;
+    let max_lines = map.iter().count();
+    let mut line_width: usize = 0;
+    while y < max_lines {
+        let line = map.iter().nth(y).unwrap();
+        if 0 == line_width {
+            line_width = line.chars().count();
+        }
+        if x > line_width {
+            x = x - line_width;
+        }
+        let char = line.chars().nth(x).unwrap();
+        if '#' == char {
+            trees += 1;
+        }
+        y += go_down;
+        x += go_right;
+    }
+    trees
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn part1_demo() {
+        assert_eq!(7, part1(&get_demo_map(), 3, 1));
+    }
+
+    fn get_demo_map() -> Vec<String> {
+        vec![
+            "..##.......".to_string(),
+            "#...#...#..".to_string(),
+            ".#....#..#.".to_string(),
+            "..#.#...#.#".to_string(),
+            ".#...##..#.".to_string(),
+            "..#.##.....".to_string(),
+            ".#.#.#....#".to_string(),
+            ".#........#".to_string(),
+            "#.##...#...".to_string(),
+            "#...##....#".to_string(),
+            ".#..#...#.#".to_string(),
+        ]
+    }
+}
