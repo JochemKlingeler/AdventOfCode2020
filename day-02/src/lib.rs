d536b4b0dc6666133e023774b93da3cb5d89b462
Jochem Klingeler <jochem.klingeler@gmail.com>
2020-12-06 11:29:50 +0100

    Found the issue, was logic error, fixed it

--- day-02/src/lib.rs
+++ day-02/src/lib.rs
@@ -1,32 +1,30 @@
 use std::convert::TryInto;
 
-pub fn part1(input: &Vec<String>) -> u16 {
-    input
-        .iter()
-        .filter(|i| is_valid(i))
-        .count()
-        .try_into()
-        .unwrap()
+pub fn part1(input: &Vec<String>) -> usize {
+    input.iter().filter(|i| is_valid(&i)).count()
 }
 
 fn is_valid(input: &String) -> bool {
-    // If only Rust had regexes, grr, there has to be an easier way
-    let mut split = input.splitn(3, ' ');
-    let amount_rules = split.next().unwrap();
-    let mut amount_rules_split = amount_rules.split('-');
-    let min = amount_rules_split.next().unwrap().parse::<u8>().unwrap();
-    let max = amount_rules_split.next().unwrap().parse::<u8>().unwrap();
-    // TODO: Future me, this char is empty string? Fix me!
-    let char = split.next().unwrap().chars().nth(0).unwrap();
-    let password = split.next().unwrap();
+    let mut main_split = input.splitn(3, ' ');
+    let (rules, char_to_find, password) = (
+        main_split.next().unwrap(),
+        main_split.next().unwrap(),
+        main_split.next().unwrap(),
+    );
+    let mut rules_split = rules.splitn(2, '-');
+    let (min, max) = (
+        rules_split.next().unwrap().parse::<u8>().unwrap(),
+        rules_split.next().unwrap().parse::<u8>().unwrap(),
+    );
+    let char = char_to_find.chars().nth(0).unwrap();
 
-    let char_count = password
+    let char_count: u8 = password
         .chars()
         .filter(|c| c == &char)
         .count()
         .try_into()
         .unwrap();
-    min >= char_count || char_count <= max
+    min >= char_count && char_count <= max
 }
 
 #[cfg(test)]
