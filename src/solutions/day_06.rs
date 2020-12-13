d4d33f0bd41f506e5fda92e04ff1f416c5e7606b
Jochem Klingeler <jochem.klingeler@gmail.com>
2020-12-13 21:01:43 +0100

    Day 6, part 1 done, part 2 wip

--- src/solutions/day_06.rs
+++ src/solutions/day_06.rs
@@ -1,11 +1,25 @@
+use std::collections::HashMap;
 use std::fs;
 
 pub fn part1() -> usize {
     do_part1(&get_day_06_input())
 }
 
-fn do_part1(_input: &str) -> usize {
-    0
+fn do_part1(input: &str) -> usize {
+    input
+        .split("\n\n")
+        .map(|f| {
+            let mut vec = f
+                .chars()
+                .filter(|c| !c.is_whitespace())
+                .collect::<Vec<char>>();
+
+            vec.sort();
+            vec.dedup();
+            vec.len()
+        })
+        .into_iter()
+        .sum()
 }
 
 pub fn part2() -> usize {
@@ -13,7 +27,20 @@ pub fn part2() -> usize {
 }
 
 fn do_part2(_input: &str) -> usize {
-    0
+    // Idea, in the hashmap we store how many times each char was given
+    // and then count the items that have a count the same as linecount.
+    // This way we know all passengers have given the same answers.
+    input
+        .split("\n\n")
+        .map(|f| {
+            let mut char_count = HashMap::new();
+            let lines = f.lines();
+            for line in lines {
+                line.chars()
+            }
+        })
+        .into_iter()
+        .sum()
 }
 
 fn get_day_06_input() -> String {
@@ -24,12 +51,28 @@ fn get_day_06_input() -> String {
 mod tests {
     use super::*;
 
+    const DEMO: &str = "abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b";
+
     #[test]
     fn part1_demo() {
-        assert_eq!(2, 2);
+        assert_eq!(11, do_part1(DEMO));
     }
     #[test]
     fn part2_demo() {
-        assert_eq!(2, 2);
+        assert_eq!(6, do_part2(DEMO));
     }
 }
